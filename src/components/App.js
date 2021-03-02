3feb245a4d47387e7dddc2733441c3ac55af6b8c
Joelle Everaert <72984240+Joelle-Everaert@users.noreply.github.com>
2021-03-02 16:39:00 +0100

    API/SCSS OK

--- src/components/App.js
+++ src/components/App.js
@@ -2,47 +2,50 @@ import { Component } from 'react';
 import '../sass/app.scss'
 
 
-
-
-
 class App extends Component {
   state = {   // définition d'un state
-  name:{}
+  data:[],
+  loading: false
 }
 
 componentDidMount(){
-  fetch('https://anapioficeandfire.com/api/characters/583')
+  this.setState({
+    loading:true
+  })
+
+  fetch('https://anapioficeandfire.com/api/houses')
     .then((response) => {  //fonction call back
       return response.json()  // recupère en format json
     })
     .then((result) => {
       console.log(result)
-      this.setState({name: result})
+      this.setState({
+        data: result,
+        loading: false
+      })
     })
   }
   
   render() {
-    if(this.state.name.aliases === undefined){
-      return <div className="title">Loading beeetch</div>
+    if(this.state.data.length === 0 && this.state.loading == false){
+      return <div className="title">No result</div>
     }
+    if(this.state.loading == true){
+      return <div className="title">Loading ........</div>
+    }
+
     return (
       <div className="title">
-        <h1>GAME OF THRONE</h1>
-        <p>Name:{this.state.name.name}</p>
-
-        <p>Gender: {this.state.name.gender}</p>
-        <p>Titles: {this.state.name.titles}</p>
-        <ul>
-          Aliases:
-          {this.state.name.aliases.map((alias)=>(
-            <li>{alias}</li>
-          ))}
-          <br/>
-          Tv Serie:
-          {this.state.name.tvSeries.map((tv)=>(
-            <li>{tv}</li>
-          ))}
-        </ul>
+        <h1>GAME OF THRONE HOUSES</h1>
+        <div className="container">
+        {this.state.data.map(house=>(
+          <div className="card">
+            <h2>{house.name}</h2>
+            <p>{house.coatOfArms}</p>
+            <h3>Region: {house.region}</h3>
+            </div>
+        ))}
+        </div>
       </div>
     );
   }
